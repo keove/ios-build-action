2447bafc24dc0e117b8d88d5e30a787c734f96a9
enesbakis <48660542+enesbakis@users.noreply.github.com>
2023-03-15 15:28:31 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -18,11 +18,18 @@ fi
 
 echo "Dir list"
 
+echo "/Users/runner/"
+ls /Users/runner/
+echo "/Users/runner/work/"
+ls /Users/runner/work
+echo "/Users/runner/work/_actions"
+ls /Users/runner/work/_actions
+echo "/Users/runner/work/trippy-cid-test/"
+ls /Users/runner/work/trippy-cid-test/
 echo "/Users/runner/work/trippy-cid-test/trippy-cid-test/"
 ls /Users/runner/work/trippy-cid-test/trippy-cid-test/
 
-echo "/Users/runner/work/trippy-cid-test/"
-ls /Users/runner/work/trippy-cid-test/
+
 
 
 # If the variable FASTLANE_ENV is set then run fastlane with the --env equal to the variable.
