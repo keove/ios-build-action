7a641ada6e3adc8b9c016650624eaa7bbb01e311
Enes <enesbakis@gmail.com>
2023-03-21 22:14:50 +0300

    Update

--- build.sh
+++ build.sh
@@ -1,14 +1,14 @@
 #!/bin/bash -e
-
+echo "Deneme Folder : " $(cd $(dirname ${0}); pwd)
 # Dizinimizi bir değişkene atıyoruz
-directory="/Users/runner/work/_actions/keove/ios-build-action/v2.2.6/dist/../"
+directory="/Users/runner/work/_actions/keove/ios-build-action/v2.2.7/dist/../"
 
 # build.sh dosyasını ekleyerek tam dizin oluşturuyoruz
 full_path="${directory}discord.sh"
  
 
 #install fastlane
-bash "$full_path" "$GITHUB_REPOSITORY [${GITHUB_REF##*/}] #$GITHUB_RUN_NUMBER" "Fastlane kuruluyor" "Trippy-Dev-Android" "True" "Fastlane kuruluyor" "16711680" 
+bash "$full_path" "$GITHUB_REPOSITORY [${GITHUB_REF##*/}] #$GITHUB_RUN_NUMBER" "Fastlane kuruluyor" "Trippy-Dev-ios" "True" "Fastlane kuruluyor" "0x00ff00" 
 
 if ! type fastlane > /dev/null 2>&1; then
   if type brew > /dev/null 2>&1; then
@@ -19,7 +19,7 @@ if ! type fastlane > /dev/null 2>&1; then
 fi
 
 #install bundler
-bash "$full_path" "$GITHUB_REPOSITORY [${GITHUB_REF##*/}] #$GITHUB_RUN_NUMBER" "Bundler kuruluyor" "Trippy-Dev-Android" "0" "Bundler kuruluyor" "16711680"
+bash "$full_path" "$GITHUB_REPOSITORY [${GITHUB_REF##*/}] #$GITHUB_RUN_NUMBER" "Bundler kuruluyor" "Trippy-Dev-ios" "0" "Bundler kuruluyor" "0xffff00"
 script_path=$(cd $(dirname ${0}); pwd)
 cp -r ${script_path}/fastlane ./
 cp -r ${script_path}/Gemfile ./
@@ -34,16 +34,16 @@ if [ -n "${FASTLANE_ENV}" ]; then
     echo "Running fastlane with environment: ${FASTLANE_ENV}"
     if ! type bundle > /dev/null 2>&1; then
         echo "Bundler is not installed. Please install bundler to use fastlane with environment variables."
-        bash "$full_path" "$GITHUB_REPOSITORY [${GITHUB_REF##*/}] #$GITHUB_RUN_NUMBER" "Bundler kurulamadı" "Trippy-Dev-Android" "0" "Bundler kurulamadı" "16711680"
+        bash "$full_path" "$GITHUB_REPOSITORY [${GITHUB_REF##*/}] #$GITHUB_RUN_NUMBER" "Bundler kurulamadı" "Trippy-Dev-ios" "0" "Bundler kurulamadı" "0xffff00"
     fi
     
     if ! type fastlane > /dev/null 2>&1; then
         echo "Fastlane is not installed. Please install fastlane to use fastlane with environment variables."
-        bash "$full_path" "$GITHUB_REPOSITORY [${GITHUB_REF##*/}] #$GITHUB_RUN_NUMBER" "Fastlane kurulamadı" "Trippy-Dev-Android" "0" "Fastlane kurulamadı" "16711680"
+        bash "$full_path" "$GITHUB_REPOSITORY [${GITHUB_REF##*/}] #$GITHUB_RUN_NUMBER" "Fastlane kurulamadı" "Trippy-Dev-ios" "0" "Fastlane kurulamadı" "0xffff00"
     fi
     fastlane --env ${FASTLANE_ENV} build
 else
     echo "Running fastlane"
-    bash "$full_path" "$GITHUB_REPOSITORY [${GITHUB_REF##*/}] #$GITHUB_RUN_NUMBER" "Fastlane çalıştırılıyor" "Trippy-Dev-Android" "0" "Fastlane çalıştırılıyor" "16711680"
+    bash "$full_path" "$GITHUB_REPOSITORY [${GITHUB_REF##*/}] #$GITHUB_RUN_NUMBER" "Fastlane çalıştırılıyor" "Trippy-Dev-ios" "0" "Fastlane çalıştırılıyor" "0xffff00"
     fastlane build
 fi
